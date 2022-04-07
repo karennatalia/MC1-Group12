817a3623cea28a5077bb65a96d5b6387070db41b
Karen Natalia <karen.natalia888@gmail.com>
2022-04-07 11:21:56 +0700

    Initial Commit

---
+++ MC1-Group12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC1-Group12
+//
+//  Created by Karen Natalia on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
