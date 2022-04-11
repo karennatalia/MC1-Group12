234c160cb23e8c8fafb101c90625305d76c71542
Karen Natalia <karen.natalia888@gmail.com>
2022-04-11 15:25:57 +0700

    merge main

--- MC1-Group12/ViewController.swift
+++ MC1-Group12/ViewController.swift
@@ -10,9 +10,26 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var welcomeLabel: UILabel!
+    func getWelcomeTime() {
+        let hour = Calendar.current.component(.hour, from: Date())
+       
+        switch hour {
+            case 6..<12 :
+            welcomeLabel.text = "Good " +  NSLocalizedString("Morning", comment: "Morning")
+            case 12..<15 :
+            welcomeLabel.text = "Good " +  NSLocalizedString("Afternoon", comment: "Afternoon")
+            case 15..<18 :
+            welcomeLabel.text = "Good " +  NSLocalizedString("Evening", comment: "Evening")
+            default:
+            welcomeLabel.text = "Good " +  NSLocalizedString("Night", comment: "Night")
+        }
+    }
+
     let notifCenter = UNUserNotificationCenter.current()
     
     override func viewDidLoad() {
+        getWelcomeTime()
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
