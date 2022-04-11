6f8ec2904052e1a1b6f7b6e922dbef7481afa117
Robby Suliantoro <robby.suliantoro@gmail.com>
2022-04-11 11:33:27 +0700

    fix activity details

--- MC1-Group12/ViewController.swift
+++ MC1-Group12/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         notifCenter.requestAuthorization(options: [.alert, .sound]) { granted, error in
             if granted == true {
