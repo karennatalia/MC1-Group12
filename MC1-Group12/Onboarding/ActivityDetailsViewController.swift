5a319e4f9e9e98768309b90b80cd391a2d7ab3cf
Rizki Ahmad Faris <rizkiahf@gmail.com>
2022-04-12 10:30:13 +0700

    feat: detail controller

--- MC1-Group12/Onboarding/ActivityDetailsViewController.swift
+++ MC1-Group12/Onboarding/ActivityDetailsViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ActivityDetailsViewController: UIViewController {
 
+    @IBOutlet weak var detailTitle: UITextView!
+    @IBOutlet weak var detailAge: UIButton!
+    @IBOutlet weak var detailPreparation: UIButton!
+    @IBOutlet weak var detailDuration: UIButton!
+    @IBOutlet weak var detailDesc: UITextView!
+    @IBOutlet weak var detailReflect: UITextView!
+    
+    var selectedAct : ActivityClass!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        detailTitle.text = selectedAct.title
+        detailAge.setTitle(selectedAct.age, for: .normal)
+        detailPreparation.setTitle(selectedAct.preparation, for: .normal)
+        detailDuration.setTitle(selectedAct.duration, for: .normal)
+        detailDesc.text = selectedAct.description
+        detailReflect.text = selectedAct.reflection
+        
         // Do any additional setup after loading the view.
     }
     
