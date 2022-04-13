99d2f860ed117ae35999b052395fcd4cdd3dd7fc
rizk19 <49632285+rizk19@users.noreply.github.com>
2022-04-13 08:01:27 +0700

    Revert "Eki/feature/copyact list to detail"

--- MC1-Group12/ViewController.swift
+++ MC1-Group12/ViewController.swift
@@ -10,14 +10,6 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var ActTableView: UITableView!
-    
-    var activityList = [ActivityClass]()
-    var filteredActList = [ActivityClass]()
-    var isSearching = false
-    
-    @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var welcomeLabel: UILabel!
     func getWelcomeTime() {
         let hour = Calendar.current.component(.hour, from: Date())
@@ -37,12 +29,8 @@ class ViewController: UIViewController {
     let notifCenter = UNUserNotificationCenter.current()
     
     override func viewDidLoad() {
-        activityList = ActivitySeeder().generateActivity()
         getWelcomeTime()
         super.viewDidLoad()
-        ActTableView?.delegate = self
-        ActTableView.dataSource = self
-        searchBar.delegate = self
         
         // Do any additional setup after loading the view.
         notifCenter.requestAuthorization(options: [.alert, .sound]) { granted, error in
@@ -63,71 +51,4 @@ class ViewController: UIViewController {
     }
     
 }
-//
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        if isSearching {
-            return filteredActList.count
-        }
-        return activityList.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let actCell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellAct") as! TableViewCell
-        
-        if isSearching {
-            actCell.ActivityTitle?.text = filteredActList[indexPath.section].title
-            actCell.ActivityAge?.text = filteredActList[indexPath.section].age
-            actCell.ActivityDuration.setTitle(filteredActList[indexPath.section].duration, for: .normal)
-            actCell.ActivityDuration.titleLabel?.font = UIFont(name: "SF Pro", size: 8)
-            actCell.ActivityPreparation.setTitle(filteredActList[indexPath.section].preparation, for: .normal)
-        } else {
-            actCell.ActivityTitle?.text = activityList[indexPath.section].title
-            actCell.ActivityAge?.text = activityList[indexPath.section].age
-            actCell.ActivityDuration.setTitle(activityList[indexPath.section].duration, for: .normal)
-            actCell.ActivityDuration.titleLabel?.font = UIFont(name: "SF Pro", size: 8)
-            actCell.ActivityPreparation.setTitle(activityList[indexPath.section].preparation, for: .normal)
-        }
-        return actCell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "toActivityDetail", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toActivityDetail" {
-            let indexPath = self.ActTableView.indexPathForSelectedRow!
-            let tableViewDetail = segue.destination as? ActivityDetailsViewController
-            
-            if isSearching {
-                tableViewDetail?.selectedAct = filteredActList[indexPath.section]
-            } else {
-                tableViewDetail?.selectedAct = activityList[indexPath.section]
-            }
-            
-            self.ActTableView.deselectRow(at: indexPath, animated: true)
-        }
-    }
-}
-
-extension ViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print("searchtext", searchText)
-//        filteredActList = activityList.filter({$0.title.prefix(searchText.count) == searchText})
-        filteredActList = activityList.filter { text in
-            return text.title.lowercased().contains(searchText.lowercased())
-       
-        }
-        if filteredActList.count == 0 {
-            filteredActList = activityList
-        }
-        isSearching = true
-        ActTableView.reloadData()
-    }
-}
